d3a5a1f4344885bacdb89a62052b77f4ffdadbe7
罗坤 <luokun199503@qq.com>
2022-01-11 15:00:53 +0800

    fix: react-natve-web __DEV__ not defined

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,14 @@
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
+const webpack = require('webpack')
 
 module.exports = function override(config, env) {
   config.resolve.plugins = config.resolve.plugins.filter(
     (plugin) => !(plugin instanceof ModuleScopePlugin),
   )
+
+  config.plugins.push(new webpack.DefinePlugin({
+    __DEV__: JSON.stringify(env.mode !== 'production'),
+  }))
 
   return config
 }
